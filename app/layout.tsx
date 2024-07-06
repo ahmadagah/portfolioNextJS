7c03d34956e31d68bf72d98cefad06b017faf1de
ahmad <zafarsafi98@gmail.com>
2024-07-06 07:40:09 -0700

    deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,38 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 
-import "./globals.css";
-import { ThemeProvider } from "./provider";
+import './globals.css'
+import { ThemeProvider } from './provider'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
-};
+  title: 'CodeCraft by PDX98',
+  description:
+    'Discover innovative web solutions by PDX98 in Portland. Explore cutting-edge projects and modern web development techniques in this portfolio',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html
+      lang='en'
+      suppressHydrationWarning
+    >
       <head>
-        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+        <link
+          rel='icon'
+          href='/jsm-logo.png'
+          sizes='any'
+        />
       </head>
       <body className={inter.className}>
         <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
+          attribute='class'
+          defaultTheme='dark'
           enableSystem
           disableTransitionOnChange
         >
@@ -32,5 +40,5 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
